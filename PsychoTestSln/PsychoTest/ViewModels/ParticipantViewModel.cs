9124ef2f25b8a22a51d2fc30d9527b8cf7f1cfce
Алексей Перовский <perovskijab@gmail.com>
2016-03-13 19:49:30 +0300

    files where not added

---
+++ PsychoTestSln/PsychoTest/ViewModels/ParticipantViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+
+namespace PsychoTest.ViewModels
+{
+    public class ParticipantViewModel : ViewModelBase
+    {
+        private string _firstName;
+        private string _secondName;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                Set(ref _firstName, value);
+                this.RaisePropertyChanged(() => this.FullName);
+            }
+        }
+
+        public string SecondName
+        {
+            get { return _secondName; }
+            set
+            {
+                Set(ref _secondName, value);
+                this.RaisePropertyChanged(() => this.FullName);
+            }
+        }
+
+        public string FullName => $"{SecondName} {FirstName}";
+
+        public bool NotEmpty => !string.IsNullOrEmpty(FirstName)
+                                && !string.IsNullOrEmpty(SecondName);
+    }
+}
---
+++ PsychoTestSln/PsychoTest/ViewModels/TabViewModelBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+
+namespace PsychoTest.ViewModels
+{
+    public class TabViewModelBase : ViewModelBase
+    {
+        private bool _selected;
+
+        public bool Selected
+        {
+            get { return _selected; }
+            set { Set(ref _selected, value); }
+        }
+    }
+}
